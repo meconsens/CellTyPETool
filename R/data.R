896b46f17c779b98ba66df81bc4e1779c348129e
Micaela Consens <micaela.consens@mail.utoronto.ca>
2020-11-15 22:44:18 -0500

    Added first dataset

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Marker Genes for the Marker Gene Profile method
+#'
+#' Marker Genes from the BRETIGEA packaged reformated for use in the Marker Gene Profile method
+#'
+#' @source BRETIGEA
+#'
+#' @format A nested list with 6 sub-lists of marker genes for brain cells: astrocyte, endothelial, microglia, neuron, oligodnedrocyte and oligodendrocyte precursor cells:
+#' \describe{
+#'  \item{ast}{Top 50 astrocyte marker genes.}
+#'  \item{end}{Top 50 astrocyte marker genes.}
+#'  \item{mic}{Top 50 microglia marker genes.}
+#'  \item{neu}{Top 50 neuron marker genes.}
+#'  \item{oli}{Top 50 oligodendrocyte marker genes.}
+#'  \item{opc}{Top 50 oligodendrocyte precursor cell marker genes.}
+#' }
+#' @examples
+#' \dontrun{
+#'  mgp_cell_markers
+#' }
+"mgp_cell_markers"
+
+# [END]
